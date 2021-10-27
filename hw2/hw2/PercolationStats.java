01417a7f195b4f2f2636d9d50ae6bc9fb8f3d6f1
m42532461 <m42532461@gmail.com>
2021-10-27 11:27:34 +0800

    complete constructor and help method calSum()

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -2,36 +2,42 @@ package hw2;
 import edu.princeton.cs.introcs.*;
 
 public class PercolationStats {
+    private int T;
+    private double[] openSiteFractions;
+    private double sum;
 
-    private int row;
-    private int expTime;
-    private PercolationFactory pf;
-    private double sumOfValue;
-    /*
-    Constructor
-    perform T independent experiments on an N-by-N grid
-     */
+    // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
-        this.row = N;
-        this.expTime = T;
-        this.pf = pf;
-    }
-
-    public double calSum() {
-        Percolation a = pf.make(row);
-        while (!a.percolates()) {
-            a.open(0,1);
-            StdRandom
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
         }
-        this.sumOfValue += a.size;
-        return 0;
+        this.T = T;
+        openSiteFractions = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = pf.make(N);
+            while (!percolation.percolates()) {
+                int x, y;
+                do {
+                    x = StdRandom.uniform(N);
+                    y = StdRandom.uniform(N);
+                } while (percolation.isOpen(x, y));
+                percolation.open(x, y);
+            }
+            openSiteFractions[i] = (double) percolation.numberOfOpenSites() / (N * N);
+        }
+    }
+    public double calSum() {
+        for (double i : openSiteFractions) {
+            sum += i;
+        }
+        return sum;
     }
 
     /*
     sample mean of percolation threshold
      */
     public double mean() {
-        return 0;
+        return  calSum() / T;
     }
     /*
     sample standard deviation of percolation threshold
