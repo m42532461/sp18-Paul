6887a5ef10d8f8e61c18673f07f02275ea1e1dff
KunQi <m42532461@gmail.com>
2021-10-30 10:58:19 +0800

    Complete bfs(int v) and solve() but still hava some bug

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.Iterator;
+
 /**
  *  @author Josh Hug
  */
@@ -9,25 +14,52 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         maze = m;
-        int s = maze.xyTo1D(sourceX, sourceY);
-        int t = maze.xyTo1D(targetX, targetY);
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[s] = true;
+        announce();
+        Queue q = new edu.princeton.cs.algs4.Queue<Integer>();
+        q.enqueue(s);
+
+        while (!q.isEmpty()) {
+            s = (int)q.dequeue();
+            if (s == t) {
+                targetFound = true;
+            }
+
+            if (targetFound) {
+                return;
+            }
+            for (int i : maze.adj(s)){
+                if (!marked[i]) {
+                    q.enqueue(i);
+                    marked[i] = true;
+                    edgeTo[i] = s;
+                    announce();
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs(s);
     }
 }
 
